83c848a8ace15fbff8ff0c5fe6521f69f09a1240
stepan <stepa.korobov@gmail.com>
2024-10-17 13:26:05 +0400

    added test for post wallet

--- test/test_main.py
+++ test/test_main.py
@@ -40,7 +40,7 @@ async def test_add_user_to_db():
 
 
 async def test_get_wallet_response(ac: AsyncClient):
-    """"""
+    """Тест отдачи баланса кошелька"""
     response = await ac.get("/api/v1/wallets/111")
     expected_response = {
         "wallet_uuid": "111",
@@ -52,11 +52,89 @@ async def test_get_wallet_response(ac: AsyncClient):
 
 
 async def test_get_wallet_not_found(ac: AsyncClient):
-    """"""
+    """Тест отдачи, если кошелёк не найден"""
     response = await ac.get("/api/v1/wallets/666")
     expected_response = {
         "error": "wallet 666 not found."
     }
 
     assert response.status_code == 404
+    assert response.json() == expected_response
+
+
+async def test_post_wallet_deposit_response(ac: AsyncClient):
+    """Тест пополнения баланса кошелька"""
+    response = await ac.post("/api/v1/wallets/111/operation",
+                             json={"operationType": "DEPOSIT", "amount": 1000})
+    expected_response = {
+        "wallet_uuid": "111",
+        "balance": 1000,
+    }
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+async def test_post_wallet_withdraw_response(ac: AsyncClient):
+    """Тест снятия с баланса кошелька"""
+    response = await ac.post("/api/v1/wallets/111/operation",
+                             json={"operationType": "WITHDRAW", "amount": 1000})
+    expected_response = {
+        "wallet_uuid": "111",
+        "balance": 0,
+    }
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+async def test_post_wallet_operation_not_exist(ac: AsyncClient):
+    """Тест на несуществующие операции с кошельком"""
+    response = await ac.post("/api/v1/wallets/111/operation",
+                             json={"operationType": "UPDATING", "amount": 1000})
+    expected_response = {
+        'error': 'Invalid operation type.'
+    }
+
+    assert response.status_code == 400
+    assert response.json() == expected_response
+
+
+async def test_post_wallet_not_found(ac: AsyncClient):
+    """Тест отдачи, если кошелёк не найден"""
+    response = await ac.post("/api/v1/wallets/666/operation",
+                             json={"operationType": "DEPOSIT", "amount": 1000})
+    expected_response = {
+        "error": "wallet 666 not found."
+    }
+
+    assert response.status_code == 404
+    assert response.json() == expected_response
+
+
+async def test_post_wallet_insufficient_funds(ac: AsyncClient):
+    """Тест отдачи, если на кошелёке отрицательный баланс"""
+    response = await ac.post("/api/v1/wallets/111/operation",
+                             json={"operationType": "WITHDRAW", "amount": 1000})
+    expected_response = {
+        "error": "Insufficient funds."
+    }
+
+    assert response.status_code == 400
+    assert response.json() == expected_response
+
+
+async def test_post_wallet_incorrect_amount(ac: AsyncClient):
+    """Тест отдачи (валидатора), при некоректном типе данных amount"""
+    response = await ac.post("/api/v1/wallets/111/operation",
+                             json={"operationType": "DEPOSIT", "amount": 'abc'})
+    expected_response = {'detail': [{'type': 'int_parsing', 'loc': ['body', 'amount', 'int'],
+                                     'msg': 'Input should be a valid integer, unable to parse string as an integer',
+                                     'input': 'abc', 'url': 'https://errors.pydantic.dev/2.9/v/int_parsing'},
+                                    {'type': 'float_parsing', 'loc': ['body', 'amount', 'float'],
+                                     'msg': 'Input should be a valid number, unable to parse string as a number',
+                                     'input': 'abc', 'url': 'https://errors.pydantic.dev/2.9/v/float_parsing'}],
+                         'body': {'operationType': 'DEPOSIT', 'amount': 'abc'}}
+
+    assert response.status_code == 422
     assert response.json() == expected_response
